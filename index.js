773295c4c10745de51bab95754a2918c84345aa0
AngelRubio9 <125904705+AngelRubio9@users.noreply.github.com>
2023-02-24 16:40:04 -0600

    Actualizacion clase Feb 24
    Clase introductoria a JavaScript, se incluyen ejemplos de interacciones del usario con la pagina Web.

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//document.write("Hello Wolrd");
+//alert("Hello Wolrd");
+var number = 33;
+var number2 = 10;
+var result = number - number2;
+//document.write(number);
+let name = "Angel ";
+//document.writeln("La suma del numero: " + number + " mas el numero: " + number2 + " es:" + (number + number2) );
+//document.writeln(" La resta del numero: " + number + " mas el numero: " + number2 + " es:" + (number - number2) );
+//alert(name);
+
+let tengoTrabajo = true;
+
+if(tengoTrabajo)
+	document.write("Actualmente tengo trabajo, gracias por visitar mi pagina Web");
+else
+	document.write("Estoy abierto a oportunidades laborales");
