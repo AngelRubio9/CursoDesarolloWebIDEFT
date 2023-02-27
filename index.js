1fb913a205b4a5b9178cf052ffadd526b771984e
AngelRubio9 <125904705+AngelRubio9@users.noreply.github.com>
2023-02-27 11:20:13 -0600

    Javascript fix
    se añade el uso de multiples funciones de JS

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 
-//document.write("Hello Wolrd");
-//alert("Hello Wolrd");
+//alert("");
 var number = 33;
 var number2 = 10;
 var result = number - number2;
 //document.write(number);
 let name = "Angel ";
-//document.writeln("La suma del numero: " + number + " mas el numero: " + number2 + " es:" + (number + number2) );
-//document.writeln(" La resta del numero: " + number + " mas el numero: " + number2 + " es:" + (number - number2) );
-//alert(name);
+
+if(window.location.pathname.includes("practicas"))
+{
+	document.getElementById('demo').innerHTML =  "La suma del numero: " + number + " mas el numero: " + number2 + " es:" + (number + number2) + "<br>" ;
+}
 
 let tengoTrabajo = true;
-
-if(tengoTrabajo)
-	document.write("Actualmente tengo trabajo, gracias por visitar mi pagina Web");
-else
-	document.write("Estoy abierto a oportunidades laborales");
+if(window.location.pathname.includes("index"))
+{
+	if(tengoTrabajo)
+		document.getElementById('CV').innerHTML = "Actualmente tengo trabajo, gracias por visitar mi pagina Web.";
+	else
+		document.getElementById('CV').innerHTML = "Estoy abierto a oportunidades laborales.";
+	
+	const experiences = document.getElementsByClassName("experience");
+	for (var i = 0; i <experiences.length; i++)
+	{
+		const item = experiences[i];
+		item.addEventListener("mouseenter", function (event)
+		{
+			event.target.style = "background-color: #999999";
+		});
+		item.addEventListener("mouseleave", function (event)
+		{
+			event.target.style = "";
+		});
+	}
+}
